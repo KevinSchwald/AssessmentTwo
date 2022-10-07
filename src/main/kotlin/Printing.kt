910105f0e87ac306f59fe229a2305d5680070155
Kevin Schwald <kevin.schwald@students.bfh.ch>
2022-10-07 15:49:41 +0200

    Added the Printing Methodes and startet with a Reader Class

---
+++ src/main/kotlin/Printing.kt
@@ -0,0 +1,30 @@
+class Printing {
+    fun printHeader() {
+        print(
+            """
+**********************************************     
+****** Welcome to Red Rocket Gas Station******
+**********************************************
+"""
+        )
+    }
+
+    fun printMenu() {
+        print(
+            """
+**Press(1) to print the consumption per year**
+**Press(2) to print the consumption per year**
+"""
+        )
+    }
+
+    fun printFooter() {
+        print(
+            """
+********************************************** 
+********************************************** 
+********************************************** 
+"""
+        )
+    }
+}
---
+++ src/main/kotlin/Reader.kt
@@ -0,0 +1,8 @@
+import java.io.BufferedReader
+import java.io.FileReader
+
+class Reader {
+    fun loadCSV(path: String) {
+        val bufferedReader = BufferedReader(path);
+    }
+}
