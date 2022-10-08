ec47a2427f755aa2b265669937be0bc29e8a1128
Kevin Schwald <kevin.schwald@students.bfh.ch>
2022-10-08 16:21:53 +0200

    Added the ability to read a CSV

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,60 @@
+import java.io.File
+import java.io.InputStream
+
 fun main(args: Array<String>) {
-    Printing().printHeader()
-    Printing().printMenu()
-    Printing().printFooter()
+    readCSV(createStream("src/main/resources/gasreading.csv"))
+    printHeader()
+    printMenu()
+    printFooter()
+}
+fun printHeader() {
+    print(
+        """
+**********************************************     
+****** Welcome to Red Rocket Gas Station******
+**********************************************
+"""
+    )
+}
+
+fun printMenu() {
+    print(
+        """
+**Press(1) to print the consumption per year**
+**Press(2) to print the consumption per year**
+"""
+    )
+}
+
+fun printFooter() {
+    print(
+        """
+********************************************** 
+********************************************** 
+********************************************** 
+"""
+    )
+}
+
+data class Data (
+    val beginDate: String,
+    val endDate: String,
+    val beginIndex: Int,
+    val endIndex: Int,
+    val kpcs: Double
+        )
+
+fun createStream(path: String): InputStream {
+    return File(path).inputStream()
+}
+
+fun readCSV(input: InputStream): List<Data> {
+    val reader = input.bufferedReader()
+    reader.readLine() // to cut the first line out
+    return reader.lineSequence()
+        .filter { it.isNotBlank() }
+        .map {
+            val(beginDate, endDate, beginIndex, endIndex, kpcs) = it.split(',', ignoreCase = false, limit = 5)
+            Data(beginDate, endDate, beginIndex.toInt(), endIndex.toInt(), kpcs.toDouble())
+        }.toList()
 }
--- src/main/kotlin/Reader.kt
+++
@@ -1,8 +0,0 @@
-import java.io.BufferedReader
-import java.io.FileReader
-
-class Reader {
-    fun loadCSV(path: String) {
-        val bufferedReader = BufferedReader(path);
-    }
-}
