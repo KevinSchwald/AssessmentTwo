a6458fd7ab3e4613960195694685ae1e30f68346
Kevin Schwald <kevinschwald@macbook-pro-von-kevin.home>
2022-10-29 17:17:03 +0200

    added some comments for the functions and also a try/catch block for the csvReader function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,9 @@ import java.io.InputStream
 import java.time.LocalDate
 import java.time.Month
 import java.time.format.DateTimeFormatter
+import kotlin.math.roundToInt
 
+// The main function in which the CLI runs until exit == true
 fun main(args: Array<String>) {
 
     val data = readCSV(createStream("src/main/resources/gasreading.csv"))
@@ -24,7 +26,7 @@ fun main(args: Array<String>) {
                         try {
                             println("if you want to get the consumption of the chosen month enter m3 or kwh")
                             val inputConsumption: String? = readlnOrNull()
-                            if (inputConsumption == "m3" ||  inputConsumption == "kwh"){
+                            if (inputConsumption == "m3" || inputConsumption == "kwh") {
                                 println("${calcMonth(getMonth(inputMonth, data)!!, inputConsumption)} $inputConsumption")
                             } else {
                                 println("please only enter m3 or kwh")
@@ -99,6 +101,7 @@ fun printFooter() {
     )
 }
 
+// Two functions called pretty which get a List as Input and are converting the raw List into e readable String by using a String Template
 fun pretty(input: MonthData?): String {
     return "Period from ${input!!.beginDate} - ${input.endDate}, beginning at ${input.beginIndex} and ending at ${input.endIndex} the conversion ratio is ${input.kpcs}"
 }
@@ -111,6 +114,7 @@ fun pretty(input: List<MonthData>): String {
     return output
 }
 
+// A data class callrd MonthData because Month is already in use in Kotlin, it stores the information of exactly one month
 data class MonthData(
         val beginDate: LocalDate,
         val endDate: LocalDate,
@@ -119,40 +123,52 @@ data class MonthData(
         val kpcs: Double
 )
 
+// A function called createStream which converts the csv to a InputStream so that the function readCSV can work with the data provided
 fun createStream(path: String): InputStream {
     return File(path).inputStream()
 }
 
+// A function called readCSV which gets a InputStream as parameter and maps the stream to the coresponding fields in the data class MonthData
 fun readCSV(input: InputStream): List<MonthData> {
     val reader = input.bufferedReader()
     reader.readLine() // to cut the first line out
-    return reader.lineSequence()
-            .filter { it.isNotBlank() }
-            .map {
-                val (beginDate, endDate, beginIndex, endIndex, kpcs) = it.split(',', ignoreCase = false, limit = 5)
-                MonthData(
-                        beginDate = LocalDate.parse(beginDate, DateTimeFormatter.ofPattern("dd/MM/yyyy")),
-                        endDate = LocalDate.parse(endDate, DateTimeFormatter.ofPattern("dd/MM/yyyy")),
-                        beginIndex.toInt(),
-                        endIndex.toInt(),
-                        kpcs.toDouble()
-                )
-            }.toList()
+    try {
+        return reader.lineSequence()
+                .filter { it.isNotBlank() }
+                .map {
+                    val (beginDate, endDate, beginIndex, endIndex, kpcs) = it.split(',', ignoreCase = false, limit = 5)
+                    MonthData(
+                            beginDate = LocalDate.parse(beginDate, DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+                            endDate = LocalDate.parse(endDate, DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+                            beginIndex.toInt(),
+                            endIndex.toInt(),
+                            kpcs.toDouble()
+                    )
+                }.toList()
+    } catch (e: Exception) {
+        return emptyList()
+    } finally {
+        reader.close()
+    }
 }
 
+// A function called getMonth which returns a month as MonthData out of a List<MonthData>
+// Tis function is used to display the chosen month and is used to get chosen months to calculate
 fun getMonth(input: String?, data: List<MonthData>): MonthData? {
     var result: MonthData? = null
     val temp = input!!.split("/")
     val month = Month.of(temp[0].toInt())
     val year = temp[1].toInt()
     data.forEach {
-        if ((it.beginDate.month == month) && (it.beginDate.year == year)) {
+        if ((it.beginDate.month == month-1) && (it.beginDate.year == year)) {
             result = it
         }
     }
     return result
 }
 
+// A function which returns a List<MonthData> out of another List<MonthData>
+// This Function is used to display the chosen year and is used to get the chosen year to calculate
 fun getYear(year: String?, data: List<MonthData>): List<MonthData> {
     val result = mutableListOf<MonthData>()
     data.forEach {
@@ -163,18 +179,19 @@ fun getYear(year: String?, data: List<MonthData>): List<MonthData> {
     return result
 }
 
-
+// This function gets a MonthData object and the chosen calculaten as input and calculates either the m3 or kwh
 fun calcMonth(month: MonthData, calc: String): Int {
     var result = 0
     if (calc == "m3") {
         result = month.endIndex - month.beginIndex
     }
     if (calc == "kwh") {
-        result = ((month.endIndex - month.beginIndex) * month.kpcs).toInt()
+        result = ((month.endIndex - month.beginIndex) * month.kpcs).roundToInt()
     }
     return result
 }
 
+// This function gets a List<MonthData> and returns a String with every calculation done
 fun calcYear(data: List<MonthData>): String {
 
     var total = 0
@@ -189,11 +206,11 @@ fun calcYear(data: List<MonthData>): String {
         consumption += calcMonth(it, "kwh")
         if (max < calcMonth(it, "kwh")) {
             max = calcMonth(it, "kwh")
-            maxMonth = it.beginDate.month
+            maxMonth = it.endDate.month
         }
         if (min > calcMonth(it, "kwh")) {
             min = calcMonth(it, "kwh")
-            minMonth = it.beginDate.month
+            minMonth = it.endDate.month
         }
     }
 
@@ -201,5 +218,5 @@ fun calcYear(data: List<MonthData>): String {
             "the total Consumption was $consumption, " +
             "the max consumption was $max in the month of $maxMonth, " +
             "the min consumption was $min in the month of $minMonth " +
-            "and the average consumption in the chosen year was ${consumption / total}."
+            "and the average consumption in the chosen year was ${consumption / total}kwh."
 }
